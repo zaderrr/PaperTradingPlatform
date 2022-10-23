6f7080a15ecfa0e06fe3960f8a6a4e0e9c66fda9
zaderrr <ellisbrown88@gmail.com>
2022-10-23 12:04:00 +0100

    bug fix, prices were passed at strings

--- frontend/src/Components/TradingComponents/ChartComp.js
+++ frontend/src/Components/TradingComponents/ChartComp.js
@@ -16,13 +16,14 @@ componentDidMount(){
     if (prevProp.StockData !== this.props.StockData){
       console.log(this.props.StockData)
       var data= []
+      
       for (let index = 0; index < this.props.StockData.length; index++) {
         var point = {
           name : index,//new Date(this.props.StockData[index]["Time"] * 1000).toDateString(),
-          High : this.props.StockData[index]["High"],
-          Low : this.props.StockData[index]["Low"],
-          Open : this.props.StockData[index]["Open"],
-          Close : this.props.StockData[index]["Close"]
+          High : parseFloat(this.props.StockData[index]["High"]),
+          Low : parseFloat(this.props.StockData[index]["Low"]),
+          Open : parseFloat(this.props.StockData[index]["Open"]),
+          Close : parseFloat(this.props.StockData[index]["Close"])
         }
         data.push(point)
       }
@@ -34,10 +35,10 @@ componentDidMount(){
   render() {
     return (
     <div id="GraphBox"style={{height:'100%'}}>
-        {this.state.CanDrawGraph && <ResponsiveContainer width="100%" height="80%">
+        {this.state.CanDrawGraph && <ResponsiveContainer width="100%" height="100%">
         <LineChart  data={this.state.Data}>
           <XAxis  dataKey="name" />
-          <YAxis dataKey="Close" type="number"  domain={['dataMin', 'dataMax']}/>
+          <YAxis domain={['auto', 'auto']}  />
           <Tooltip 
            contentStyle={{"color" : 'red'}}
            itemStyle={{"color" : 'blue'}}
